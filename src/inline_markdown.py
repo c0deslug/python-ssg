4798eff8789036a5be4d9e4411ab3843444eb2c4
c0deslug <s.rados37@gmail.com>
2025-07-09 20:17:55 +0200

    added way to split link and image nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -32,6 +32,78 @@ def extract_markdown_links(text):
     return matches
     
 
+def split_nodes_image(old_nodes):
+    newTNode = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            newTNode.append(node)
+            continue
+        
+        original_text = node.text
+        images = extract_markdown_images(original_text)
+
+        if len(images) == 0:
+            newTNode.append[node]
+            continue
+
+        for image in images:
+            
+            image_alt = image[0]
+            image_link = image[1]
+            sections = original_text.split(f"![{image_alt}]({image_link})", 1)
+            
+            if len(sections) != 2:
+                raise ValueError("Markdown not closed")
+            
+            original_text = sections[1]
+            
+            if sections[0] != "":
+                newTNode.append(TextNode(sections[0], TextType.TEXT))    
+            newTNode.append(TextNode(image_alt, TextType.IMAGE, image_link))
+        
+        if original_text != "":
+            newTNode.append(TextNode(original_text, TextType.TEXT))
+    
+    return newTNode   
+
+
+
+def split_nodes_link(old_nodes):
+    newTNode = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            newTNode.append(node)
+            continue
+        
+        original_text = node.text
+        links = extract_markdown_links(node.text)
+
+        if len(links) == 0:
+            newTNode.append[node]
+            continue
+
+        for link in links:
+            
+            link_alt = link[0]
+            link_url = link[1]
+            sections = original_text.split(f"[{link_alt}]({link_url})", 1)
+            
+            if len(sections) != 2:
+                raise ValueError("Markdown not closed")
+            
+            original_text = sections[1]
+            
+            if sections[0] != "":
+                newTNode.append(TextNode(sections[0], TextType.TEXT))
+
+            newTNode.append(TextNode(link_alt, TextType.LINK, link_url))
+        
+        if original_text != "":
+            newTNode.append(TextNode(original_text, TextType.TEXT))
+    
+    return newTNode   
+    pass
+
 
 
 
